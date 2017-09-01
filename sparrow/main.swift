b704026be67b9715611c6de267d7ac38a1fb481e
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2017-09-01 09:49:31 -0700

    Initial Commit

---
+++ sparrow/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sparrow
+//
+//  Created by Ahmad Alhashemi on 2017-09-01.
+//  Copyright © 2017 Ahmad Alhashemi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
